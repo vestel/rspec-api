f86379a7d7d76ffa5e952bec0045cd20b9ce6c9c
Aleksandrs Zdancuks <vestel@blog.copperred.net>
2016-06-02 17:45:43 +0300

    Add complex case :null or :object notation
    In case
        has_attributes :attr_name, type: [:null, :object] do 
          has_attributes :params, type: string
        end
    validation was failing. Now it is working,

--- lib/rspec-api/resource/has_attribute.rb
+++ lib/rspec-api/resource/has_attribute.rb
@@ -6,12 +6,21 @@ module RSpecApi
     module HasAttribute
       def has_attribute(name, options, &block)
         if block_given?
-          options[:type] = Hash[options[:type], {}]
+          options[:type] = Hash[options[:type], {}] unless options[:type].is_a? Array
           nest_attributes options[:type], &Proc.new
         end
         if @attribute_ancestors.present?
-          hash = @attribute_ancestors.last
-          hash.each{|type, _| (hash[type] ||= {})[name] = options}
+          collection = @attribute_ancestors.last    # Hash and Array possible
+          collection.each do |type, _|
+            if collection.is_a? Array
+              if type == :object
+                index = collection.find_index(type)       # Update array
+                collection[index] = {type => { name => options}}
+              end
+            else
+              (collection[type] ||= {})[name] = options # Old behaviour (Hash provided)
+            end
+          end
         else
           (rspec_api_resource[:attributes] ||= {})[name] = options
         end
